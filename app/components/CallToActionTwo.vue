62f91f9e47a471837e5845cc3b8f1d0dfbbf8a55
bengtsalez <henrik@wolkano.se>
2026-05-06 08:50:19 +0200

    Update CallToActionTwo.vue

--- app/components/CallToActionTwo.vue
+++ app/components/CallToActionTwo.vue
@@ -48,6 +48,79 @@
       </div>
     </div>
   </div>
+
+        <UForm
+          class="bg-neutral-200 flex flex-col gap-4 p-6 shadow-md border-b-3 border-primary-600"
+        >
+          <h2 class="font-semibold text-2xl">Få en kostnadsfri offert</h2>
+          <u-form-field label="DITT NAMN">
+            <u-input
+              label="Namn"
+              placeholder="Erik Andersson"
+              variant="ghost"
+              size="xl"
+              class="w-full border-b border-neutral-600"
+              autocomplete="name"
+            />
+          </u-form-field>
+          <u-form-field label="E-POST">
+            <u-input
+              label="E-post"
+              placeholder="erik@exempel.se"
+              variant="ghost"
+              size="xl"
+              class="w-full border-b border-neutral-600"
+              autocomplete="email"
+            />
+          </u-form-field>
+          <u-form-field label="TELEFONNUMMER">
+            <u-input
+              label="Telefonnummer"
+              placeholder="070-000 00 00"
+              variant="ghost"
+              size="xl"
+              class="w-full border-b border-neutral-600"
+              autocomplete="tel"
+            />
+          </u-form-field>
+          <u-form-field label="Gatuadress">
+            <u-input
+              label="Gatuadress"
+              placeholder="Storgatan 1"
+              variant="ghost"
+              size="xl"
+              class="w-full border-b border-neutral-600"
+              autocomplete="street-address"
+            />
+          </u-form-field>
+          <u-form-field label="Postnummer">
+            <u-input
+              label="Postnummer"
+              placeholder="201 45"
+              variant="ghost"
+              size="xl"
+              class="w-full border-b border-neutral-600"
+              autocomplete="postal-code"
+            />
+          </u-form-field>
+          <u-form-field label="Stad">
+            <u-input
+              label="Stad"
+              placeholder="Malmö"
+              variant="ghost"
+              size="xl"
+              class="w-full border-b border-neutral-600"
+              autocomplete="address-level2"
+            />
+          </u-form-field>
+          <UButton
+            label="SKICKA FÖRFRÅGAN"
+            class="flex justify-center gradient-btn py-4 text-2xl"
+          />
+          <p class="w-full text-center text-sm">
+            Vi återkommer till dig inom 12 timmar. Din information behandlas konfidentiellt och används endast för att ge dig en offert.
+          </p>
+        </UForm>
 </template>
 
 <script setup lang="ts"></script>
